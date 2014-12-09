2ad5896e48c8597f1ae797ca9f611e59fc60455f
felixkosmalla <felixkosmalla@gmail.com>
2014-12-08 21:07:44 -0800

    added sphere

---
+++ Unity/test2/Assets/Modifytest.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class Modifytest : MonoBehaviour {
+
+	TerrainData td;
+
+	private float[,] old_heights;
+
+
+
+	// Use this for initialization
+	void Start () {
+		TerrainCollider tdc;
+		tdc = (TerrainCollider) GetComponent ("TerrainCollider");
+		td = tdc.terrainData;
+
+
+		old_heights = td.GetHeights(0,0,tdc.terrainData.heightmapWidth, tdc.terrainData.heightmapHeight);
+
+
+		print("test");
+	}
+
+
+	void modifyT(){
+
+		float[,] heights = td.GetHeights (0, 0, 200, 200);
+
+		for (int x = 0; x < 200; x++) {
+			for(int y = 0; y < 200; y++){
+				heights[x,y] = Mathf.Min(heights[x,y] * 1.1f, 1.0f);
+			}		
+		
+		}
+
+		//heights [0, 0] = 1;
+
+		td.SetHeights (0, 0, heights);
+
+		
+	}
+
+	void OnDestroy(){
+		// reset the old terrani
+		td.SetHeights (0, 0, old_heights);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space")) {
+			print ("space key was pressed");
+
+			var timer = System.Diagnostics.Stopwatch.StartNew();
+			modifyT();
+			timer.Stop();
+			var elapsed = timer.Elapsed;
+			print (elapsed.Milliseconds);
+
+		}
+	}
+}
---
+++ Unity/test2/Assets/Scripts/ManipulatorControl.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class ManipulatorControl : MonoBehaviour {
+
+	private HandController hc;
+
+	public GameObject positionIndicator;	
+	public Terrain terrain;
+
+	private TerrainCollider tc;
+	private TerrainData td;
+
+	// Use this for initialization
+	void Start () {
+		hc = (HandController) GetComponent("HandController");
+
+		tc = (TerrainCollider) terrain.GetComponent("TerrainCollider");
+		td = tc.terrainData;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// see if we have a hand
+
+		if (hc.GetAllGraphicsHands ().Length == 0)
+			return;
+		
+		HandModel hand = hc.GetAllGraphicsHands()[0];
+		
+		// get the palm positions
+
+		// this is the absolute position in unity coordinates
+		Vector3 palmPosition = hand.GetPalmPosition();
+
+
+		// get the delta between the handcontroller and the palm
+		Vector3 controllerHandDelta = hc.transform.position - palmPosition;
+
+
+		// calculate the new position for the position indicator
+		Vector3 newIndicatorPosition = new Vector3 ();
+		newIndicatorPosition.x = hc.transform.position.x + controllerHandDelta.x * -40.0f;
+		newIndicatorPosition.z = hc.transform.position.z + controllerHandDelta.z * -40.0f;
+		newIndicatorPosition.y = positionIndicator.transform.localPosition.y;
+
+
+
+		// get the position in terrain coordinates
+		Vector3 posInTerrainCoordinates = newIndicatorPosition - terrain.transform.position;
+
+		posInTerrainCoordinates = posInTerrainCoordinates * (1.0f/td.size.x) * (float) td.heightmapWidth;
+
+		print (posInTerrainCoordinates);
+
+
+		// try to get the height value
+		float terrainHeightNormalized = td.GetHeight((int) posInTerrainCoordinates.x, (int) posInTerrainCoordinates.z);
+
+		// scale it
+		float terrainHeightAbsolut = terrainHeightNormalized * td.size.y;
+		
+		newIndicatorPosition.y = terrainHeightNormalized +80;
+		print (terrainHeightNormalized);
+		positionIndicator.transform.position = newIndicatorPosition;
+
+
+
+		//print (palmPosition);
+
+	}
+}
